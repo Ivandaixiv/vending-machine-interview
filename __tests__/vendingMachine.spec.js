009183fb824330767c05b68aa63db0dbd9b3017c
Ivan <ivandaixivwork@gmail.com>
2020-02-27 11:23:56 -0800

    Adds the basic structure for the vending machine

---
+++ __tests__/vendingMachine.spec.js
@@ -0,0 +1,9 @@
+const vendingMachine = require("../lib/vendingMachine");
+
+describe("vendingMachine", () => {
+  describe("", () => {
+    it("", () => {
+      expect().toEqual();
+    });
+  });
+});
