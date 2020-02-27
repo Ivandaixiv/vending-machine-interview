97fd21dac3cf3d3f2d730ad3cac7a15464a9f4ad
Ivan <ivandaixivwork@gmail.com>
2020-02-27 11:55:42 -0800

    Adds returning products and coins from the vending machine

--- __tests__/vendingMachine.spec.js
+++ __tests__/vendingMachine.spec.js
@@ -3,6 +3,11 @@ const vendingMachine = require("../lib/vendingMachine");
 describe("vendingMachine", () => {
   describe("", () => {
     it("", () => {
+      const vendingMachine1 = new vendingMachine(
+        ["Coke", "Pepsi"],
+        ["25", "5"]
+      );
+      console.log(vendingMachine1.productsInventory);
       expect().toEqual();
     });
   });
