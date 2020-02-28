07d19a09855af8a5e22fb4a0063bdb27d7d479a9
Ivan <ivandaixivwork@gmail.com>
2020-02-27 16:30:11 -0800

    Adds paying for products and calculating the change needed to be dispensed

--- __tests__/vendingMachine.spec.js
+++ __tests__/vendingMachine.spec.js
@@ -1,14 +1,30 @@
-const vendingMachine = require("../lib/vendingMachine");
+const VendingMachine = require("../lib/VendingMachine");
 
-describe("vendingMachine", () => {
-  describe("", () => {
-    it("", () => {
-      const vendingMachine1 = new vendingMachine(
-        ["Coke", "Pepsi"],
-        ["25", "5"]
-      );
-      console.log(vendingMachine1.productsInventory);
-      expect().toEqual();
+const inventory = require("../mockData.json");
+let sodaVendingMachine;
+
+describe("Vending Machine", () => {
+  beforeEach(() => {
+    sodaVendingMachine = new VendingMachine(inventory);
+  });
+  describe("print inventory", () => {
+    it("should return the inventory", () => {
+      expect(sodaVendingMachine.getInventory).toBe(inventory);
+    });
+  });
+  describe("dispenses a product and caculate change if user pays exact amount", () => {
+    it("should return chips and 30 change", () => {
+      expect(sodaVendingMachine.payForProduct("A1", 180)).toEqual([
+        "Chips",
+        30
+      ]);
+    });
+  });
+  describe("does not dispense anything if user hasn't inserted the required amount", () => {
+    it("should return an error", () => {
+      expect(() => {
+        sodaVendingMachine.payForProduct("A2", 150);
+      }).toThrow();
     });
   });
 });
